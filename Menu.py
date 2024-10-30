7feda568aaf7c5095168bbf394aacc20a2fdc280
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-10-30 13:06:22 +0100

    Updated menu

--- Menu.py
+++ Menu.py
@@ -1,85 +1,109 @@
-
 def menu():
     print('Welcome to the menu!')
-
     print("1. Display users.")
     print("2. Add user.")
     print("3. Remove user.")
     print("4. Exit the program.")
 
-def loop():
+def choices():
     while True:
-            menu()
-            choice = input('Which item from the menu interests you? ').strip()
+        menu()
+        choice = input('Which item from the menu interests you? ').strip()
 
-            if not choice.isdigit():
-                print("Enter a number from the range of 1 to 4.")
-                continue
+        if not choice.isdigit():
+            print("Enter a number from the range of 1 to 4.")
+            continue
 
-            choice = int(choice)
+        choice = int(choice)
 
-            if choice == 1:
-                choice_one()
-            elif choice == 2:
-                choice_two()
-            elif choice == 3:
-                choice_three()
-            elif choice == 4:
-                break
-            else:
-                print("The menu only has items from 1 to 4.")
-def choice_one():
-         with open("Users from program - Menu.txt", "r") as file:
-            users = file.readlines()
+        if choice == 1:
+            display_users()
+        elif choice == 2:
+            add_user()
+        elif choice == 3:
+            remove_user()
+        elif choice == 4:
+            print("Exiting the program. Goodbye!")
+            break
+        else:
+            print("The menu only has items from 1 to 4.")
+
+def display_users():
+    with open("Users from program - Menu.txt", "a+") as file:
+        file.seek(0)  # Move to the start of the file
+        content = file.readlines()
+        
+        if len(content) == 0:
+            answer = int(input("File is empty. Do you want to add a user to the list? If yes, enter - 1: "))
+            if answer == 1:
+                print("You can add user names by selecting option 2.\n")
+                choices()
+        else:
             print("Here is the list of users:")
-            for user in users:
+            for user in content:
                 print(user.strip())
-def choice_two():
+        
+        print()
+        back_to_menu = int(input("Would you like to return to the menu? Enter 1 for yes: "))
+        if back_to_menu == 1:
+            choices()
 
-         with open("Users from program - Menu.txt", "r") as file:
+def add_user():
+    while True:
+        new_user = input("Which user's name would you like to add? ").strip()
+        with open("Users from program - Menu.txt", "a+") as file:
+            file.seek(0)
             users = file.readlines()
-            new_user = input("Enter the name of the user you want to add:").strip()
+
             if new_user + '\n' in users:
-                print("A user with that name already exists.")
+                another_name = input("A user with that name already exists. You must choose another username.\n"
+                                     "Enter another name or return to menu by entering 1: ")
+                if another_name.isdigit() and int(another_name) == 1:
+                    choices()
+                else:
+                    continue
             else:
-                with open("Users from program - Menu.txt", 'a') as file:
-                    file.write(new_user + '\n')
-                    print(f'User added {new_user}!')
-                    users.append(new_user)
-                    print("Here is the list of users:")
-                    for user in users:
-                        print(user.strip())
-def choice_three():
+                file.write(new_user + '\n')
+                print(f'User added: {new_user}!')
+                users.append(new_user + '\n')
 
-        with open("Users from program - Menu.txt", "r") as file:
-            users = file.readlines()
+                print("Here is the updated list of users:")
+                for user in users:
+                    print(user.strip())
+
+                next_user = int(input("Do you want to add the next user to the list? If yes enter - 1, enter - 2 if you would like to return to the menu: "))
+                if next_user == 1:
+                    continue
+                else:
+                    back_to_menu = int(input("Would you like to return to the menu? Enter 1 for yes: "))
+                    if back_to_menu == 1:
+                        choices()
+
+def remove_user():
+    with open("Users from program - Menu.txt", "r") as file:
+        users = file.readlines()
+        
+        if not users:
+            print("The user list is empty.")
+            return
+        
+        print("Here is the list of users:")
+        for user in users:
+            print(user.strip())
+        
+        remove_user = input("Enter the name of the user you want to delete: ").strip()
+        if remove_user + '\n' in users:
+            users.remove(remove_user + '\n')
+            with open('Users from program - Menu.txt', 'w') as file:
+                file.writelines(users)
+            print(f'User deleted: {remove_user}!')
+        else:
+            print("There is no user with that name in the list.")
+
+        user_list = input("If you want to display the list of users again, type 'yes': ")
+        if user_list.lower() == 'yes':
             print("Here is the list of users:")
             for user in users:
                 print(user.strip())
-                remove_user = input("Enter the name of the user you want to delete:").strip()
-                if remove_user + '\n' in users:
-                    users.remove(remove_user + '\n')
-                    with open('Users from program - Menu.txt', 'w') as file:
-                        file.writelines(users)
-                    print(f'User deleted{remove_user}!')
-                    print("Here is the list of users:")
-                    for user in users:
-                        print(user.strip())
 
-                else:
-                    print("There is no user with that name in the menu.")
-                    user_list =(input("If you want to display the list of users, type 'yes'."))
-                    print("Here is the list of users:")
-                    if user_list == 'yes':
-                        for user in users:
-                            print(user.strip())
-
-loop()
-
-
-
-
-
-
-
-
+choices()
---
+++ New_Menu.py
@@ -0,0 +1,85 @@
+
+def menu():
+    print('Witaj w menu!')
+
+    print("1. Wyświetl użytkowników.")
+    print("2. Dodaj użytkownika.")
+    print("3. Usuń użytkownika")
+    print("4. Wyjdź z programu.")
+
+def loop():
+    while True:
+            menu()
+            choice = input('Który punkt z menu Cie interesuje? ').strip()
+
+            if not choice.isdigit():
+                print("Musisz podać cyfre od 1 do 4.")
+                continue
+
+            choice = int(choice)
+
+            if choice == 1:
+                choice_one()
+            elif choice == 2:
+                choice_two()
+            elif choice == 3:
+                choice_three()
+            elif choice == 4:
+                break
+            else:
+                print("W menu są tylko punkty od 1 do 4.")
+def choice_one():
+         with open("Uzytkownicy z programu - menu.txt", "r") as file:
+            users = file.readlines()
+            print("Oto lista użytkowników:")
+            for user in users:
+                print(user.strip())
+def choice_two():
+
+         with open("Uzytkownicy z programu - menu.txt", "r") as file:
+            users = file.readlines()
+            new_user = input("Wpisz imie użytkownika, którego chcesz dodać: ").strip()
+            if new_user + '\n' in users:
+                print("Użytkownik o takiej nazwie już istnieje.")
+            else:
+                with open("Uzytkownicy z programu - menu.txt", 'a') as file:
+                    file.write(new_user + '\n')
+                    print(f'Dodano użytkownika {new_user}!')
+                    users.append(new_user)
+                    print("Oto lista użytkowników:")
+                    for user in users:
+                        print(user.strip())
+def choice_three():
+
+        with open("Uzytkownicy z programu - menu.txt", "r") as file:
+            users = file.readlines()
+            print("Oto lista użytkowników:")
+            for user in users:
+                print(user.strip())
+                remove_user = input("Wpisz imię użytkownika, którego chcesz usunąć: ").strip()
+                if remove_user + '\n' in users:
+                    users.remove(remove_user + '\n')
+                    with open('Uzytkownicy z programu - menu.txt', 'w') as file:
+                        file.writelines(users)
+                    print(f'Usunięto użytkownika {remove_user}!')
+                    print("Oto lista użytkowników:")
+                    for user in users:
+                        print(user.strip())
+
+                else:
+                    print("Użytkownika o takiej nazwie nie ma w menu.")
+                    user_list =(input("Jeżeli chcesz wyświetlić listę użytkówników wpisz 'tak' "))
+                    print("Oto lista użytkowników:")
+                    if user_list == 'tak':
+                        for user in users:
+                            print(user.strip())
+
+loop()
+
+
+
+
+
+
+
+
