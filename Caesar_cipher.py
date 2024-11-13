d395b4a22239d99c67f9a44f675bf93d2342ad1b
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-13 21:09:23 +0100

    Changed the alphabet display, modified the encode and decode functions, and added conditions to the while loop.

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,35 +1,33 @@
-print("CAESAR CIPHER") #wpisac w princie wielkie litery, w zmiennej alphabet to samo
-print('''The program supports the following characters:
-"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F","g",
-"G","h","H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M",
-"n", "N", "o"," O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", 
-"T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z",
-"!", "+", "-", "*", "@", "=".''')
-print("Characters that are not letters will not be encoded.\n")
-
+print("CAESAR CIPHER")
 special_char = ["!", "+", "-", "*", "@", "="]
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h",
             "i", "j", "k", "l", "m", "n", "o", "p",
             "q", "r", "s", "t", "u", "v", "w", "x",
             "y", "z"]
+uppercase_alphabet = [letter.upper() for letter in alphabet]
+print('The program supports the following characters: \n', alphabet, "\n", uppercase_alphabet, "\n", special_char)
+
+print("Characters that are not letters will not be encoded.\n")
 
 def encode(secret_message, shifting):
     encode_message = ''
-    try:
-        for char in secret_message:
-            if char.lower() in alphabet:
-                is_upper = char.isupper()
-                char_lower = char.lower()
-                idx = alphabet.index(char_lower)
-                new_idx = (idx + shifting) % len(alphabet)
-                new_char = alphabet[new_idx]
-                if is_upper:
-                    new_char = new_char.upper()
-                encode_message += new_char
-            elif char in special_char:
-                encode_message += char
-    except ValueError:
-        print("The program does not support this character.")
+
+    for char in secret_message:
+        if char.lower() in alphabet:
+            is_upper = char.isupper()
+            char_lower = char.lower()
+            idx = alphabet.index(char_lower)
+            new_idx = (idx + shifting) % len(alphabet)
+            new_char = alphabet[new_idx]
+            if is_upper:
+                new_char = new_char.upper()
+            encode_message += new_char
+        elif char in special_char:
+            encode_message += char
+        else:
+            print("The program does not support this character.")
+            encode_message = None
+            break
     return encode_message
 
 
@@ -45,8 +43,12 @@ def decode(secret_message, secret_code):
             if is_upper:
                 new_char = new_char.upper()
             decode_message += new_char
-        else:
+        elif char in special_char:
             decode_message += char
+        else:
+            print("The program does not support this character.")
+            decode_message = None
+            break
     return decode_message
 
 def type_message_code():
@@ -65,11 +67,13 @@ while True:
     if choice == "encode":
         secret_message, shifting = type_message_code()
         encode_message = encode(secret_message, shifting)
-        print(f"Here's the encoded result: {encode_message}")
+        if encode_message != None:
+            print(f"Here's the encoded result: {encode_message}")
     elif choice == "decode":
         secret_message, shifting = type_message_code()
         decode_message = decode(secret_message, shifting)
-        print(f"Here's the decoded result: {decode_message}")
+        if decode_message != None:
+            print(f"Here's the decoded result: {decode_message}")
     else:
         print("You must enter encode or decode, nothing else.")
         continue
