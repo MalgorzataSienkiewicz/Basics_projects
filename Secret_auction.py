70b552de54ae082606f169b517721d986040eaaf
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-16 22:50:58 +0100

    Final version

--- Secret_auction.py
+++ Secret_auction.py
@@ -1,16 +1,19 @@
 import random
 
 bids = {}
-
 items = ["Mona Lisa image", "Phone Nokia 3310", "Pokemon card with Pikachu"]
+def draw():
+    if len(items) > 0:
+        auction_item = random.choice(items)
+        items.remove(auction_item)
+        return auction_item
+    return None
 
-
-def welcome():
+def welcome(auction_item):
     print("Welcome to the secret auction program.")
-    name = input("What is your name? ")
-    auction_item = random.choice(items)
+    name = input("What is your name? ").strip()
     print(f"Hello {name}! Today you can buy {auction_item}.")
-    return name, auction_item
+    return name
 
 def offer():
     while True:
@@ -20,51 +23,48 @@ def offer():
             print("Your bid isn't integer. Try again.")
             continue
         print(f"Your offer: {bid}")
-        for key, value in bids.items():
-            if bid <= value:
-                print("Your offer must be higher than the current offer. ")
-                break
-        else:
-            while True:
-                decision = input("Would you like to place a higher offer? Type 'yes', or press enter and finish submitting offers. ").lower()
-                if decision == "yes":
-                    break
-                elif decision == "":
-                    return bid
-                else:
-                    print("Incorrect command. Try again.")
-def start_game():
-    name, auction_item = welcome()
-    bid = offer()
-    bids[name] = bid
-    return bids, auction_item
+        break
+    return bid
 
 def find_highest_bidder(bidding_dictionary):
     winner = max(bidding_dictionary, key=bidding_dictionary.get)
     return f"The winner is {winner} with a bid of ${bidding_dictionary[winner]}."
 def main():
-    while items:
-        bids, auction_item = start_game()
+    end_auction = False
+    auction_item = draw()
+
+    while True:
+        name = welcome(auction_item)
+        bid = offer()
+        bids[name] = bid
+
         while True:
-            should_continue = input(f"Are there any other bidders? Type 'yes', or press enter and conclude the auction {auction_item}.").lower()
+            should_continue = input(f"Are there any other bidders? Type 'yes', or press enter and conclude the auction {auction_item}. ").lower()
             if should_continue == 'yes':
                 print("\n" * 100)
-
-            elif should_continue == "":
-                print(bids)
-                print(find_highest_bidder(bids))
-                print("Press Enter to exit the auction.")
                 break
             else:
-                print("Incorrect command. Try again.")
-                continue
-
-        items.remove(auction_item)
-        if items:
-            print("It's time for the next auction.")
-        else:
-            print("Thank you, everyone. We don't have any items to auction. Goodbye!")
+                print(bids)
+                print(find_highest_bidder(bids))
+                decision = input("Do yo want to continue the auction with another item? Type 'yes', or press enter to exit the auction. ")
+                if decision == "yes":
+                    auction_item = draw()
+                    if auction_item:
+                        print("It's time for the next auction.")
+                        bids.clear()
+                        break
+                    else:
+                        print("Thank you, everyone. We don't have any items to auction. Goodbye!")
+                        end_auction = True
+                        break
+                else:
+                    print("Thank you, everyone. Goodbye!")
+                    end_auction = True
+                    break
+        if end_auction == True:
             break
+
+
 main()
 
 
