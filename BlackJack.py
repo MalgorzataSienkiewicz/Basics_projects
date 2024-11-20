2c625f1d6c13b2a78b9639966ecb52525177c1cf
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-20 11:52:42 +0100

    Incomplete version - without split option

--- BlackJack.py
+++ BlackJack.py
@@ -20,32 +20,24 @@ player = []
 croupier = []
 
 def showing_cards(round):
-    if round == 1 and len(player) != 0 and len(croupier) !=0:
-        player_card = random.choice(player)  
-        print(f"One of the player's cards is: {player_card}")
-        croupier_card = random.choice(croupier)
-        print(f"One of the dealer's cards is: {croupier_card}")
+    if round == 1 and len(player) != 0 and len(croupier) !=0: 
+        print(f"One of the player's cards is: {player[0]}")
+        print(f"One of the dealer's cards is: {croupier[0]}")
     else:
         print(f"Player cards: {player}")
-        print(f"Dealer cards: {croupier}")
-    return 
-
-
-def draw(round):
-    draw_player = "yes"
-    if round == 1:
-        for i in range(4):
-            card = random.choice(list(cards.items()))
-            key, value = card
-            if i == 0 or i == 1:
-                player.append(value)
-            else:
-                croupier.append(value)
         
+    
+
+def draw(round, draw_for='player'):
+    card = random.choice(list(cards.items())) 
+    key, value = card
+    if round == 1:
+        if draw_for == "player":
+            player.append(value)
+        else:
+            croupier.append(value)
     else:
-        card = random.choice(list(cards.items()))
-        key,value = card
-        if draw_player == 'yes':
+        if draw_for == "player":
             if key == "A":
                 choice = int(input("Your card will have '1' score or '11'? ")) 
                 player.append(choice)
@@ -58,46 +50,45 @@ def draw(round):
                 else:
                     croupier.append(key[0])
             croupier.append(value)
-    return key, value 
+    
 
 def computer_move():
-    if sum(croupier) < 17 and sum(croupier) < 21:
-        draw()
-
+    while sum(croupier) < 17:
+        draw(round=0, draw_for='croupier')
 
     
 def checking():
     score_player = sum(player)
     score_croupier = sum(croupier)
-    winner = ''
     if score_player > score_croupier:
-        showing_cards(0)
-        winner = "Player win."
-    elif score_player == score_croupier:
-        showing_cards(0)
-        winner = "It's a draw."
+        print("Player bust! Dealer wins.")
+    elif score_croupier > 21:
+        print("Dealer bust! Player wins.")
+    elif score_player > score_croupier:
+        print("Players wins!")
+    elif score_player < score_croupier:
+        print("Dealer wins!")
     else:
-        showing_cards(0)
-        winner = "Croupier win."
-    return winner
+        print("It's a draw!")
+    showing_cards(0)
+    print(f"Dealer cards: {croupier}")
     
 
 def hit():
-    draw(round=0)
+    draw(round=0, draw_for='player')
     
 
 def stand():
+    computer_move()
     checking()
 
 def double_down():
-    draw()
-    checking()
+    hit()
+    stand()
 
 def split():
     pass
 
-def surrender():
-    print("You lose.")
 
 def choice_option():
     while True:
@@ -105,40 +96,37 @@ def choice_option():
         option = input("You have 5 options: hit, stand, double_down, split, surrender. What do you choose: ").lower()
         if option == "hit":
             hit()
+            if sum(player) > 21: 
+                print("Player bust! Dealer wins.") 
+                showing_cards(0)
+                break
+            continue
         elif option == "stand":
             stand()
+            break
         elif option == "double_down":
             double_down()
+            break
         elif option == "split":
             split()
         elif option == "surrender":
-            surrender()
+            print("Player surrenders. Dealer wins.")
+            break
         else:
             print("Incorrect command. Try again.")
             continue
+    return False
+
     
 def main():
     round = 1
-    draw(round)
+    for _ in range(2):
+        draw(round, draw_for= 'player')
+        draw(round, draw_for='croupier')
     showing_cards(round)
-    round = 0
-    while True:
-        draw_player = "yes"
-        if draw_player == "yes":
-            if sum(player) > 21:
-                print("You lose.")
-                break
-            else:
-                choice_option()
-                draw_player = "no"
-                if sum(player) > 21:
-                    print("You lose.")
-                    break
-        else:
-            computer_move()
-            draw_player = "yes"
-            if sum(croupier) > 21:
-                print("Computer lose.")
+
+    while sum(player) <= 21 and choice_option():
+        pass
             
 main()
     
