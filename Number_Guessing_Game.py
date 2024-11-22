43dcfebedead2ab0705a375cb98ee49158fe756e
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-22 11:25:05 +0100

    second version

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -6,85 +6,75 @@ print("I'm thinking of a number between 1 and 100.")
 number = random.randint(1,100)
 
 def checking(guess,number):
-    hint = ""
-    result = ""
     if guess > number:
-        hint = "To high."
+        return "To high."
     elif guess < number:
-        hint = "To low."
+        return"To low."
     else:
-        result = "win"
-    return hint, result
+        return "win"
 
-        
+
 def easy_level(number):
     attempts = 10
     while attempts:
         print(f"You have {attempts} attempts remaining to guess the number.")
-        guess = int(input("Make a guess: "))
-        if not isinstance(guess, int):
+        try:
+            guess = int(input("Make a guess: "))
+        except:
             print("You must type integer. Try again")
             attempts -= 1
             continue
         else:
-           result, hint  = checking(guess,number)
+           result= checking(guess,number)
            if result != "win":
-               print(f"{hint}. Guess again")
+               print(f"{result}. Guess again")
                attempts -= 1
-               continue
            else:
-               break
-    return result
+               return "win"
+    return "lose"
 
 
 def hard_level(number):
     attempts = 5
     while attempts:
         print(f"You have {attempts} attempts remaining to guess the number.")
-        guess = int(input("Make a guess: "))
-        if not isinstance(guess, int):
+        try:
+            guess = int(input("Make a guess: "))
+        except:
             print("You must type integer. Try again")
             attempts -= 1
             continue
         else:
-           result, hint = checking(guess,number)
-           print(f"{hint}. Guess again.")
+           result = checking(guess,number)
+           print(f"{result}. Guess again.")
            if result != "win":
                attempts -= 1
-               continue
            else:
-               break
-    return result
+               return "win"
+    return "lose"
     
 
 def choose_difficulty(number):
     while True:
         difficulty = input("Type 'easy' or 'hard': ").lower().strip()
-        if isinstance(difficulty, str):
-            if difficulty == "easy":
-                easy_level(number)
-            elif difficulty == "hard":
-                hard_level(number)
-            else:
-                print("Incorrect command. Try again.")
-                continue
+        if difficulty == "easy":
+             return easy_level(number)
+        elif difficulty == "hard":
+            return hard_level(number)
         else:
             print("Incorrect command. Try again.")
-            continue
 
-
+    
 def main():
     while True:
         number = random.randint(1,100)
-        while True:
-            result = choose_difficulty(number)
-            if result == "win":
-                print("Congratulation! You win!")
-                break
-            else:
-                print("You lose.")
-                break
-        next_game = input("If you want to play again type 'yes' or press enter to quit game: ").lower().strip()
+        result = choose_difficulty(number)
+        if result == "win":
+            print("Congratulation! You win!")   
+        else:
+            print(f"You lose. My number is: {number}")
+            
+        next_game = input("If you want to play again type 'yes' or press Enter to quit game: ").lower().strip()
         if next_game == "yes":
             continue
         else:
