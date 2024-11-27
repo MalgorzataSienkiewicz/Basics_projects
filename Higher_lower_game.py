12fbe248ed4b2b54f2ebea2e68b72ae8c36f4534
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-27 13:01:11 +0100

    add game function

--- Higher_lower_game.py
+++ Higher_lower_game.py
@@ -1,7 +1,6 @@
 import json
 from random import choice
 print("Welcome in higher lower game!")
-score = 0
 with open("game_data.json", "r") as file:
     content = json.load(file)
 
@@ -10,12 +9,47 @@ def celebrity_draw():
     second_person = choice(content)
     return first_person, second_person
 
-def compare():
-    first_person, second_person = celebrity_draw()
-    print(f"Compare 'A': {first_person['name']}, a {first_person['description']}, from {first_person['country']}")
+def compare(first_person, second_person):
+    print(f"Compare 'A': {first_person['name']}, a {first_person['description']}, from {first_person['country']}.")
     print("VS")
-    print(f"Compare 'B': {second_person['name']}, a {second_person['description']}, from {second_person['country']}")
-    guess = input("Who has more followers on Instagram? Type 'A' or 'B'").upper().strip()
+    print(f"Compare 'B': {second_person['name']}, a {second_person['description']}, from {second_person['country']}.")
+    while True:
+        guess = input("Who has more followers on Instagram? Type 'A' or 'B': ").upper().strip()
+        if guess == "A" or guess == "B":
+            continue
+        else:
+            print("Incorrect command.")
+    return guess
+
+def game():
+    score = 0
+    while True:
+        first_person, second_person = celebrity_draw()
+        guess = compare(first_person,second_person)
+        if guess == "A" and first_person["follower_count"] > second_person["follower_count"]:
+            score += 1
+            print(f"You're right! Current score: {score}")
+            continue
+        elif guess == "B" and second_person["follower_count"] > first_person["follower_count"]:
+            score += 1
+            print(f"You're right! Current score: {score}")
+            continue
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}" )
+            next_game = input("Do you want play again? Type 'yes' or press Enter and quit the game.").lower().strip()
+            if next_game == "yes":
+                score = 0
+                continue
+            else:
+                break
+        break
+
+
+game()
+
+
+
+
 
 
 
