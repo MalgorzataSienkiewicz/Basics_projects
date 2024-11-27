31dbf0184fd56e6957f890e86060dd218019283c
Małgorzata Sienkiewicz <golszanska@gmail.com>
2024-11-27 13:13:06 +0100

    incomplete version

--- Higher_lower_game.py
+++ Higher_lower_game.py
@@ -9,23 +9,24 @@ def celebrity_draw():
     second_person = choice(content)
     return first_person, second_person
 
-def compare(first_person, second_person):
+def guessing(first_person, second_person):
     print(f"Compare 'A': {first_person['name']}, a {first_person['description']}, from {first_person['country']}.")
     print("VS")
     print(f"Compare 'B': {second_person['name']}, a {second_person['description']}, from {second_person['country']}.")
     while True:
         guess = input("Who has more followers on Instagram? Type 'A' or 'B': ").upper().strip()
         if guess == "A" or guess == "B":
-            continue
+            return guess
         else:
             print("Incorrect command.")
-    return guess
+            continue
+def compare():
 
 def game():
     score = 0
     while True:
         first_person, second_person = celebrity_draw()
-        guess = compare(first_person,second_person)
+        guess = guessing(first_person, second_person)
         if guess == "A" and first_person["follower_count"] > second_person["follower_count"]:
             score += 1
             print(f"You're right! Current score: {score}")
@@ -35,8 +36,11 @@ def game():
             print(f"You're right! Current score: {score}")
             continue
         else:
-            print(f"Sorry, that's wrong. Final score: {score}" )
-            next_game = input("Do you want play again? Type 'yes' or press Enter and quit the game.").lower().strip()
+            print(f"""Sorry, that's wrong.
+{first_person['name']} has {first_person['follower_count']} mln followers,
+{second_person['name']} has {second_person['follower_count']} mln followers.
+Final score: {score}""" )
+            next_game = input("Do you want play again? Type 'yes' or press Enter and quit the game: ").lower().strip()
             if next_game == "yes":
                 score = 0
                 continue
